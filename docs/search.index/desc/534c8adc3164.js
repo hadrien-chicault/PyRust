6a27fe3d3b50dcaa8cfcd899754bb89a3013cc6c
hadrien-chicault <hadrien-chicault@users.noreply.github.com>
2026-02-08 15:09:24 +0000

    deploy: 4c665e4f97bb61badebb9e794f130135b4ce1473

---
+++ docs/search.index/desc/534c8adc3164.js
@@ -0,0 +1,1 @@
+rd_("BoPyRust - High-Performance Distributed Computing")
---
+++ docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["pyrust",["",[["dataframe",[],["mod.rs"]],["session",[],["mod.rs"]],["utils",[],["mod.rs"]]],["lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[106]}
